2eff638b5888b99a9837550cc1d6189cdc3322db
Tirsa St. Fort <lifelovelori@gmail.com>
2023-01-30 15:34:15 -0800

    Completed Lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector("form")
+  form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    handleTask(document.getElementById('new-task-description').value)
+    form.reset()
+  })
 });
+
+function handleTask(task){
+  let btn = document.createElement("button")
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = "x"
+  console.log(task)
+  document.getElementById("tasks").textContent = `${task} `
+  document.getElementById("tasks").appendChild(btn)
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
+
